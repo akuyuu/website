55b6e875892413ff7f84366f969aceb0f823d06f
akuyuu <78714307+akuyuu@users.noreply.github.com>
2024-11-13 13:51:15 +0100

    inital upload

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'some title for now';
+export const SITE_DESCRIPTION = 'among us in real life';
---
+++ src/content/blog/mdtests.md
@@ -0,0 +1,16 @@
+---
+title: 'Testing markdown rendering'
+description: 'what title says'
+pubDate: 'Nov 11 2024'
+---
+
+```go
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("surreal")
+}
+```
+I`ve eaten a fruit
