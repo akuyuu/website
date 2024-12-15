17c0e3ff281d067810fb9fa40b0eba55a2816700
akuyuu <78714307+akuyuu@users.noreply.github.com>
2024-12-15 22:52:54 +0100

    testing deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://amechan.rip',
+	base: 'website',
 	integrations: [mdx(), sitemap()],
 	vite: {
 		assetsInclude: ['**/*.ttf', '**/*.png'],
