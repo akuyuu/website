70822b9d583408e6a966c203937d8dce9c1c3ddf
akuyuu <78714307+akuyuu@users.noreply.github.com>
2024-12-18 16:52:39 +0100

    deploy: css inclusion test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://amechan.rip',
-	base: 'website',
+	// base: 'website',
 	integrations: [mdx(), sitemap()],
 	vite: {
 		assetsInclude: ['**/*.ttf', '**/*.png'],
